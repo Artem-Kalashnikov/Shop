00fa7567cc70af7a03479c4a85227f38b4f62220
Artem Kalashnikov <86047484+Artem-Kalashnikov@users.noreply.github.com>
2021-12-03 14:14:22 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 function DOMLoaded () {
     const switchBtns = document.querySelectorAll('[data-switch-btn]')
 
-    if (switchBtns.length) {
-        for (const switchBtn of switchBtns) {
-            switchBtn.addEventListener('click', () => {
-                const [basketBlock, orderBlock] = document.querySelectorAll('[data-switch-block]');
-
-                if (!basketBlock || !orderBlock) throw new Error("Basket or order block is undefined");
-
-                if (switchBtn.dataset.data-switch-btn === "basket") {
-                    basketBlock.classList.remove("hide");
-                    orderBlock.classList.add("hide")
-                }
-
-                if (switchBtn.dataset.data-switch-btn === "order") {
-                    orderBlock.classList.remove("hide")
-                    basketBlock.classList.add("hide");
-                }
-            })
-        }
+    for (const switchBtn of switchBtns) {
+        switchBtn.addEventListener('click', () => {
+            const [basketBlock, orderBlock] = document.querySelectorAll('[data-switch-block]');
+            
+            if (!basketBlock || !orderBlock) throw new Error("Basket or order block is undefined");
+            
+            if (switchBtn.dataset.switchBtn === "basket") {
+                orderBlock.style.display = "none";
+                basketBlock.style.display = "block";
+            }
+            
+            if (switchBtn.dataset.switchBtn === "order") {
+                basketBlock.style.display = "none";
+                orderBlock.style.display = "block";
+            }
+        })
     }
+
 }
 
 document.addEventListener('DOMContentLoaded', DOMLoaded)
